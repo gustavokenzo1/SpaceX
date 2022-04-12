0249d8a7b76096c2b3db8897db6cd60627e9bae4
gustavokenzo1 <gustavokenzo314@gmail.com>
2022-04-11 22:41:05 -0300

    change to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import "./App.css";
 import { Dashboard } from "./pages/Dashboard";
